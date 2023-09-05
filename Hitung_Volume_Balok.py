01a1c1d158a953e0a013c12dc96a50b0a3dbe810
Vrilla <143860035+Vrilla2005@users.noreply.github.com>
2023-09-05 21:59:47 +0700

    Add files via upload
    tugas pelatihan coding

---
+++ Hitung_Volume_Balok.py
@@ -0,0 +1,11 @@
+# Program Menghitung Volume Balok
+
+print("Program Menghitung Volume Balok")
+
+p = int(input("Masukkan Panjang  : "))
+l = int(input("Masukkan Lebar  : "))
+t = int(input("Masukkan Tinggi  : "))
+
+Volume = p * l * t
+print("Rumus Menghitung Volume Balok V = p x l x t")
+print("Hasil dari Hitung Volume Balok : ",Volume)
